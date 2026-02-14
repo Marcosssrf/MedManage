a33f1ec48d316be55309b5ae355578e662458162
Marcosssrf <marcosssrf@hotmail.com>
2026-02-14 00:07:29 -0300

    Mudança de Class de MedicoMaisAtendidoDTO para Record

--- src/main/java/com/clinica/dto/MedicoMaisAtendidoDTO.java
+++ src/main/java/com/clinica/dto/MedicoMaisAtendidoDTO.java
@@ -2,27 +2,8 @@ package com.clinica.dto;
 
 import java.util.UUID;
 
-public class MedicoMaisAtendidoDTO {
-
-	private UUID medicoId;
-	private String nome;
-	private Long totalConsultas;
-
-	public MedicoMaisAtendidoDTO(UUID medicoId, String nome, Long totalConsultas) {
-		this.medicoId = medicoId;
-		this.nome = nome;
-		this.totalConsultas = totalConsultas;
-	}
-
-	public UUID getMedicoId() {
-		return medicoId;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public Long getTotalConsultas() {
-		return totalConsultas;
-	}
+public record MedicoMaisAtendidoDTO(
+		UUID medicoId,
+		String nome,
+		Long totalConsultas) {
 }
